62c639778a5fe892850ebf4d4a44d3cf3cc286d4
プー <tatiutusi@outlook.jp>
2023-09-10 01:32:53 +0900

    ファイルを戻した

--- ポートフォリオ/app.js
+++ ポートフォリオ/app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // スリックスライダー
 $('#SlickSlider').slick({
     adaptiveHeight: true,
@@ -26,4 +27,34 @@ var app = new Vue({
         off: true
     }
 });
+=======
+// スリックスライダー
+$('#SlickSlider').slick({
+    adaptiveHeight: true,
+    autoplay: true,
+    centerMode: true,
+    centerPadding: '50px',
+    arrows: false,
+
+     responsive: [{
+        breakpoint: 990, // ブレイクポイントを指定
+        settings: {
+           centerMode: false, 
+        }
+    }
+
+    ]
+
+});
+// /スリックスライダー
+
+// バーガースイッチ
+var app = new Vue({
+    el: '#app',
+    data: {
+        on: false,
+        off: true
+    }
+});
+>>>>>>> 5ef3362414ec97123d807e8b12dc7d4c996e01db
 // /バーガースイッチ
